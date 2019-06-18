7fadb4f52fdaa70788e3021b1c4fb4d7e714ce2c
caikelun <caikelun@gmail.com>
2019-06-18 20:00:08 +0800

    [improve] Remove useless linker option (-fPIE) in Android.mk.

--- src/native/libxcrash_dumper/jni/Android.mk
+++ src/native/libxcrash_dumper/jni/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE           := xcrash_dumper
 LOCAL_CFLAGS           := -std=c11 -Weverything -Werror -fvisibility=hidden -fPIE
 LOCAL_C_INCLUDES       := $(LOCAL_PATH) $(LOCAL_PATH)/../../common
 LOCAL_STATIC_LIBRARIES := lzma
-LOCAL_LDLIBS           := -llog -fPIE -pie
+LOCAL_LDLIBS           := -llog -pie
 LOCAL_SRC_FILES        := $(wildcard $(LOCAL_PATH)/*.c) $(wildcard $(LOCAL_PATH)/../../common/*.c)
 include $(BUILD_EXECUTABLE)
 include $(LOCAL_PATH)/lzma/Android.mk
