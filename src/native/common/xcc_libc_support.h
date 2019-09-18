875576c443064871b0a6b906fff460bdda32465d
Sim Sun <simsun@fb.com>
2019-09-17 19:31:05 -0700

    feat(common): implement async-signal-safe libc function in xcc_libc_support

---
+++ src/native/common/xcc_libc_support.h
@@ -0,0 +1,38 @@
+// Copyright (c) 2019-present, iQIYI, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#ifndef XCC_LIBC_SUPPORT_H
+#define XCC_LIBC_SUPPORT_H 1
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+// memset(3) is not async-signal-safe, ref: http://boston.conman.org/2016/12/17.1
+void xcc_safe_memset(void* ip, char c, size_t len);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // XCC_LIBC_SUPPORT_H
