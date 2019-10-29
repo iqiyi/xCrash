805062dd8c480b13b468a3bf61257306032632e5
Sim Sun <sunsj1231@gmail.com>
2019-10-29 03:27:00 -0700

    [improve] don't link against libstdc++ on libxcrash_dumper

--- src/native/libxcrash_dumper/jni/Android.mk
+++ src/native/libxcrash_dumper/jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE           := xcrash_dumper
 LOCAL_CFLAGS           := -std=c11 -Weverything -Werror -fvisibility=hidden -fPIE
-LOCAL_LDFLAGS          := -pie
+LOCAL_LDFLAGS          := -pie -nodefaultlibs
 LOCAL_LDLIBS           := -ldl -llog
 LOCAL_STATIC_LIBRARIES := lzma
 LOCAL_C_INCLUDES       := $(LOCAL_PATH) $(LOCAL_PATH)/../../common
