a8046d39e28f83b25e3a0c38f7aac8e9ef668014
xuqingqing <xuqingqing@qiyi.com>
2020-07-17 15:04:52 +0800

    add debug so back cmd

--- src/native/install.sh
+++ src/native/install.sh
@@ -17,3 +17,19 @@ cp -f ./libxcrash_dumper/libs/armeabi-v7a/xcrash_dumper ../java/xcrash/xcrash_li
 cp -f ./libxcrash_dumper/libs/arm64-v8a/xcrash_dumper   ../java/xcrash/xcrash_lib/src/main/jniLibs/arm64-v8a/libxcrash_dumper.so
 cp -f ./libxcrash_dumper/libs/x86/xcrash_dumper         ../java/xcrash/xcrash_lib/src/main/jniLibs/x86/libxcrash_dumper.so
 cp -f ./libxcrash_dumper/libs/x86_64/xcrash_dumper      ../java/xcrash/xcrash_lib/src/main/jniLibs/x86_64/libxcrash_dumper.so
+
+version=$1
+mkdir -p ~/xcrash_libs_backup/$version/jniLibs_Debug/armeabi-v7a
+mkdir -p ~/xcrash_libs_backup/$version/jniLibs_Debug/arm64-v8a
+mkdir -p ~/xcrash_libs_backup/$version/jniLibs_Debug/x86
+mkdir -p ~/xcrash_libs_backup/$version/jniLibs_Debug/x86_64
+
+cp -f ./libxcrash/obj/local/armeabi-v7a/libxcrash.so ~/xcrash_libs_backup/$version/jniLibs_Debug/armeabi-v7a/libxcrash.so
+cp -f ./libxcrash/obj/local/arm64-v8a/libxcrash.so   ~/xcrash_libs_backup/$version/jniLibs_Debug/arm64-v8a/libxcrash.so
+cp -f ./libxcrash/obj/local/x86/libxcrash.so         ~/xcrash_libs_backup/$version/jniLibs_Debug/x86/libxcrash.so
+cp -f ./libxcrash/obj/local/x86_64/libxcrash.so      ~/xcrash_libs_backup/$version/jniLibs_Debug/x86_64/libxcrash.so
+
+cp -f ./libxcrash_dumper/obj/local/armeabi-v7a/xcrash_dumper ~/xcrash_libs_backup/$version/jniLibs_Debug/armeabi-v7a/libxcrash_dumper.so
+cp -f ./libxcrash_dumper/obj/local/arm64-v8a/xcrash_dumper   ~/xcrash_libs_backup/$version/jniLibs_Debug/arm64-v8a/libxcrash_dumper.so
+cp -f ./libxcrash_dumper/obj/local/x86/xcrash_dumper         ~/xcrash_libs_backup/$version/jniLibs_Debug/x86/libxcrash_dumper.so
+cp -f ./libxcrash_dumper/obj/local/x86_64/xcrash_dumper      ~/xcrash_libs_backup/$version/jniLibs_Debug/x86_64/libxcrash_dumper.so
