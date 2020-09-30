f9d02619b81b2a4c9f6003c16eb6dcf453fa99d7
Xu Qingqing <xuqnqn@qq.com>
2020-09-30 18:04:21 +0800

    Change version to 2.5.9-beta

--- src/java/xcrash/xcrash_lib/src/main/java/xcrash/Version.java
+++ src/java/xcrash/xcrash_lib/src/main/java/xcrash/Version.java
@@ -27,6 +27,6 @@ class Version {
     private Version() {
     }
 
-    static final String version = "2.5.7";
+    static final String version = "2.5.9";
     static final String fullVersion = "xCrash " + version;
 }
